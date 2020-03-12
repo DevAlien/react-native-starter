6346f355f03aacf7e447d3b9519d1b89ca3eb5b8
Goncalo Margalho <gsky89@gmail.com>
2020-03-12 12:42:28 +0100

    Add localization

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import {View, Text, Button} from 'react-native';
+import strings from '../Localization';
 
 function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+      <Text>{strings.hello}</Text>
       <Button
         title={'Go To second'}
         onPress={() => navigation.navigate('Second')}
